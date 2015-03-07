90df4d9e0ed5a88077fa8c3e623fda3c24b5a7ba
wgcv <gstavocevallos@gmail.com>
2015-03-06 21:34:05 -0500

    Se agrego campos necesarios

--- crashphone/tecnico/models.py
+++ crashphone/tecnico/models.py
@@ -8,7 +8,7 @@ class Tecnico(models.Model):
 	id = models.AutoField(primary_key=True)
 	nombre = models.CharField("Nombre del tecnico", max_length=150)
 	direccion = models.CharField("Directorio del tecnico", max_length=150)
-	celular = models.CharField("Celúlar", max_length=150)
+	celular = models.CharField("Celular", max_length=150)
 	lon = models.FloatField()
 	lat = models.FloatField()
 	def __str__(self):
@@ -18,10 +18,12 @@ class Cita(models.Model):
 	id = models.AutoField(primary_key=True)
 	idTecnico = models.ForeignKey(Tecnico, unique=False)
 	idCliente = models.ForeignKey(Cliente, unique=False)
-	direccion = models.CharField("Directorio del tecnico", max_length=150)
+	direccion = models.CharField("Directorio del cliente", max_length=150)
+	fecha = models.DateField("Fecha de la reparación",auto_now=False)
+	hora = models.TimeField("Hora de la reparación")
 	dedescripcionDano = models.CharField("Datos del dano", max_length=150)
 	descripcion = models.CharField("Descripcion", max_length=150)
 	lon = models.FloatField()
 	lat = models.FloatField()
 	def __str__(self):
-		return self.dedescripcionDano
+		return ('%s | %s | %s' % (self.id, self.dedescripcionDano,self.idTecnico))
